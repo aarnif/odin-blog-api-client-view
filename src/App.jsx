d6def36d1232f495d11af1d01dc67c4f893f12f6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-05-19 15:52:56 +0300

    Add routing to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import Header from "./components/Header/index.jsx";
+import Footer from "./components/Footer/index.jsx";
+
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-red-600">
-      This is a Vite + React + Tailwind CSS app!
-    </h1>
+    <>
+      <Header />
+      <div className="w-full min-h-screen flex flex-col justify-center items-center">
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/archive" element={<div>Archive</div>} />
+          <Route path="/about" element={<div>About</div>} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
